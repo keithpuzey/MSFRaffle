a68bbb76dcb7372375cff170ab3eeb3256a2458f
keithpuzey <keithpuzey@users.noreply.github.com>
2017-06-08 13:58:11 +0100

    Update AXA URL Details

--- plugins/axa.php
+++ plugins/axa.php
@@ -1,6 +1,6 @@
 <script type="text/javascript" id="ca_eum_ba" agent="browser" async
-        src="https://axa.saas.ca.com/mdo/v1/sdks/browser/BA.js"
-        data-profileUrl="https://collector.axa.saas.ca.com/api/1/urn:ca:tenantId:48B428C9-C483-B954-105A-A612C70885A2/urn:ca:appId:Raffle%20Application/profile?agent=browser"
-        data-tenantID="48B428C9-C483-B954-105A-A612C70885A2"
-        data-appID="Raffle Application"
-        data-appKey="fa732d70-269e-11e7-b5cd-994429ed59ce"></script>
+        src="https://cloud.ca.com/mdo/v1/sdks/browser/BA.js"
+        data-profileUrl="https://collector-axa.cloud.ca.com/api/1/urn:ca:tenantId:CACDEMEA01-USERSTORE/urn:ca:appId:CA%20MSF%20Raffle/profile?agent=browser"
+        data-tenantID="CACDEMEA01-USERSTORE"
+        data-appID="CA MSF Raffle"
+        data-appKey="5bd7f960-4b7f-11e7-b7c1-5b69918a7787"></script>
