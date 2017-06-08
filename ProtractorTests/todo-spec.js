b4a92573e2091003da368df37f8f05bb035bd440
keithpuzey <keithpuzey@users.noreply.github.com>
2017-06-08 13:42:48 +0100

    Create todo-spec.js

--- ProtractorTests/todo-spec.js
+++ ProtractorTests/todo-spec.js
@@ -1,7 +1,7 @@
 describe('CDBU Modern Software Factory UI Test - xxx Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://raffle-preprod.us-east-1.elasticbeanstalk.com/index.php');
+    browser.get('http://websiteurl');
     element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('Submit')).click();
     });
