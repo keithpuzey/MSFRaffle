5225143e7ab13e3ac16e8f78a1b9f5ac6f82386c
keithpuzey <keithpuzey@users.noreply.github.com>
2017-06-12 09:09:54 +0100

    Create todo-spec.js

--- EMEASiteTests/todo-spec.js
+++ EMEASiteTests/todo-spec.js
@@ -2,7 +2,7 @@ describe('CDBU Modern Software Factory UI Test - EMEA Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://emea-preprod.cdbu.io');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('I\'m In!')).click();
     });
 });
